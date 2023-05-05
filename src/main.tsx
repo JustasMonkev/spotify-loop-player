399b93539d84d56638328813d3a62cffb0997d60
JustasM <justas.monkev@gmail.com>
2023-05-05 10:56:44 +0300

    chore: test if another component is being rendered

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from "./App.tsx";
+import HelloWorld from "./test.tsx";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App/>
+        <HelloWorld/>
     </React.StrictMode>,
 )
