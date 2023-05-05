0153521ddd821bbf7471b5c52b0c579a0591361b
JustasM <justas.monkev@gmail.com>
2023-05-05 10:32:18 +0300

    feat: add gh-pages dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
-const base = process.env.NODE_ENV === 'production' ? '/spotify-loop-player/' : '/';
-
 export default defineConfig({
-    base,
+    base: "/spotify-loop-player/",
     plugins: [react()],
     build: {
         outDir: 'dist',
