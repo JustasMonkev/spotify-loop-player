dc6968084b41bc0f29f85e4c38ad8366a0c95530
JustasM <justas.monkev@gmail.com>
2023-05-05 08:25:46 +0300

    feat: deploy vite app to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+const base = process.env.NODE_ENV === 'production' ? '/<spotify-loop-player>/' : '/';
+
 export default defineConfig({
-  plugins: [react()],
+    base,
+    plugins: [react()],
 })
