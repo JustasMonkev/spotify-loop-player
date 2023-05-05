4c4323cb0bf060389a73b35fb736ea5f58533d93
JustasM <justas.monkev@gmail.com>
2023-05-05 10:42:26 +0300

    Revert "feat: add gh-pages dependency"
    This reverts commit 0153521ddd821bbf7471b5c52b0c579a0591361b.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
+const base = process.env.NODE_ENV === 'production' ? '/spotify-loop-player/' : '/';
+
 export default defineConfig({
-    base: "/spotify-loop-player/",
+    base,
     plugins: [react()],
     build: {
         outDir: 'dist',
