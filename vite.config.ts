ea4e64b113216696a824eb29b82435959938a665
JustasM <justas.monkev@gmail.com>
2023-05-05 09:37:48 +0300

    feat: update base url path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
-const base = process.env.NODE_ENV === 'production' ? '/spotify-loop-player/spotify/' : '/';
+const base = process.env.NODE_ENV === 'production' ? '/spotify-loop-player/' : '/';
 
 export default defineConfig({
     base,
