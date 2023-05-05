883c30c3cacd0f4f7dd434bfc7680aa8c8a46afd
JustasM <justas.monkev@gmail.com>
2023-05-05 08:40:10 +0300

    chore: change base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-const base = process.env.NODE_ENV === 'production' ? '/<spotify-loop-player>/' : '/';
+const base = '/';
 
 export default defineConfig({
     base,
