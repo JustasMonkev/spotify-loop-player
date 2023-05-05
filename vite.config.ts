8d6a1d1df29f62418db6ed61de40003cc923270f
JustasM <justas.monkev@gmail.com>
2023-05-05 09:07:48 +0300

    chore: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-const base = '/';
+const base = process.env.NODE_ENV === 'production' ? '/spotify-loop-player/' : '/';
 
 export default defineConfig({
     base,
     plugins: [react()],
-})
+    build: {
+        outDir: 'dist',
+    },
+    publicDir: 'public',
+});
